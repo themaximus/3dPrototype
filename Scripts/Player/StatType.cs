52a4f37b30883d570746a6ba9f0cfd8b1e803285
Maxim Herashchenko <79747103+themaximus@users.noreply.github.com>
2026-02-04 21:14:49 +0200

    Delete Scripts directory

--- Scripts/Player/StatType.cs
+++
@@ -1,8 +0,0 @@
-public enum StatType
-{
-    Health,     // Здоровье
-    Hunger,     // Сытость (Голод)
-    Thirst,     // Жажда
-    Energy,     // Бодрость (Энергия)
-    Fun         // Досуг (Развлечение)
-}
